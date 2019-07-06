159446b49ac86d0c77b9a62fa49460aa56df7857
Artur Białoskórski <bialoskorski.artur@gmail.com>
2019-07-06 15:20:11 +0200

    initial

---
+++ src/Components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+const About = () => {
+	return (
+		<div className="container">
+			<h4 className="center">About Component</h4>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+				voluptatum suscipit commodi nulla repudiandae, quaerat eaque.
+				Vitae magnam sit aspernatur velit dolorem quis dolore ullam
+				facilis? Molestias repudiandae eveniet fugiat?
+			</p>
+		</div>
+	);
+};
+export default About;
---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Home = () => {
+	return (
+		<div className="container">
+			<h4 className="center">Home Component</h4>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+				voluptatum suscipit commodi nulla repudiandae, quaerat eaque.
+				Vitae magnam sit aspernatur velit dolorem quis dolore ullam
+				facilis? Molestias repudiandae eveniet fugiat?
+			</p>
+		</div>
+	);
+};
+export default Home;
